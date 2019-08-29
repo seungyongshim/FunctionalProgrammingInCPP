f33fa447343047a3ec70b589db9008b0516de348
심승용 <seungyong.shim@mirero.co.kr>
2019-08-29 11:13:28 +0900

    count_lines_in_files_accumulate 그린

--- 1.1.CountLines/CountLines/functions_accumulate.cpp
+++ 1.1.CountLines/CountLines/functions_accumulate.cpp
@@ -4,11 +4,6 @@
 #include "functions_accumulate.h"
 using namespace std;
 
-int f(const int previous_count, const char c)
-{
-  return (c != '\n') ? previous_count : previous_count + 1;
-}
-
 vector<int> count_lines_in_files_accumulate(const vector<string>& files)
 {
   vector<int> results(files.size());
@@ -17,7 +12,7 @@ vector<int> count_lines_in_files_accumulate(const vector<string>& files)
   {
     return accumulate(s.cbegin(), s.cend(), 0, [](auto p, auto c) -> auto
     {
-      return 0;
+      return (c != '\n') ? p : p + 1;
     });
   });
 
