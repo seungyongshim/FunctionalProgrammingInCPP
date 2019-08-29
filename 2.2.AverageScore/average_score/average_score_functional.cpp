914d03f3ccf88f46f20fbb17f0372f8072f0b14b
심승용 <seungyong.shim@mirero.co.kr>
2019-08-29 10:28:21 +0900

    average_score_functional 레드 상태

---
+++ 2.2.AverageScore/average_score/average_score_functional.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+#include "average_score_functional.h"
+
+using namespace std;\
+double average_score_functional(const vector<int>& scores)
+{
+  return 0.;
+}
---
+++ 2.2.AverageScore/average_score/average_score_functional.h
@@ -0,0 +1,3 @@
+#pragma once
+
+double average_score_functional(const std::vector<int>& scores);
