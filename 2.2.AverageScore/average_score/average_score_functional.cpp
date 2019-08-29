f2cda94c2e46565e06cb728c511c36a2dac7a65a
심승용 <seungyong.shim@mirero.co.kr>
2019-08-29 10:45:31 +0900

    average_score_functional 그린

--- 2.2.AverageScore/average_score/average_score_functional.cpp
+++ 2.2.AverageScore/average_score/average_score_functional.cpp
@@ -1,8 +1,10 @@
 #include <vector>
 #include "average_score_functional.h"
+#include <numeric>
 
-using namespace std;\
+using namespace std;
+
 double average_score_functional(const vector<int>& scores)
 {
-  return 0.;
+  return accumulate(scores.cbegin(), scores.cend(), 0) / static_cast<double>(scores.size());
 }
